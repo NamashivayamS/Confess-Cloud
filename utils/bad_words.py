04e5d1e73fe11837da6d31ace0eb357a2c0bc5e2
NamashivayamS <namashivayams1@gmail.com>
2026-01-22 12:57:22 +0530

    Update bad words list and add usage disclaimer

--- utils/bad_words.py
+++ utils/bad_words.py
@@ -1,5 +1,38 @@
 BAD_WORDS = [
-    "badword1",
-    "badword2",
-    "badword3"
+    # =========================
+    # English abusive / explicit
+    # =========================
+    "fuck", "fucking", "fuckyou", "motherfucker",
+    "shit", "bullshit",
+    "bitch", "asshole", "bastard",
+    "dick", "pussy", "slut", "whore",
+    "sex", "porn", "nude", "naked",
+    "rape", "rapist", "molest", "molestation",
+    "kill", "murder", "suicide",
+  
+    # =========================
+    # Tamil bad words (Romanized)
+    # =========================
+    "thevdiya", "thevidiya", "tevdiya",
+    "ommala", "ommale",
+    "punda", "pundai",
+    "sunni",
+    "otha", "othainga", "othaingala",
+    "sootha", "soothu",
+    "poolu", "koothi",
+    "sunniya", "pundamavan",
+    "mayiru", "mayire",
+    "lavada", "lavade",
+    "thayoli", "thaayoli",
+    "kena", "kenapunda",
+    "loosu", "loosu_punda",
+
+    # =========================
+    # Tamil harassment phrases
+    # =========================
+    "ommala_poda",
+    "punda_mavan",
+    "otha_punda",
+    "sunny_punda",
+    "thevidiya_paya"
 ]
